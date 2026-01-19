bb0d5a742a86221086903fe0e280849c95e06212
Rajeev <rajeevlakshmanan003@gmail.com>
2026-01-20 02:12:17 +0530

    Themes module completed.

--- themes/models.py
+++ themes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+    name = models.CharField(max_length=25)
+    email = models.EmailField()
+    number = models.CharField(max_length=10)
+    message = models.TextField()
+    
+    def __str__(self) -> str:
+        return self.name
--- themes/views.py
+++ themes/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Feedback
+from django.contrib import messages
 
-# Create your views here.
 def index(request):
     return render(request, 'index.html')
 
@@ -8,6 +9,21 @@ def about(request):
     return render(request, 'about.html')
 
 def feedback(request):
+    if request.method == 'POST':
+        try:
+            name = request.POST['name']
+            email = request.POST['email']
+            number = request.POST['number']
+            message = request.POST['message']
+            feedback_data = Feedback.objects.create(name=name, email=email, number=number, message=message)
+            feedback_data.save()
+            success_msg = "Thank you for your Feedback"
+            messages.success(request, success_msg)
+            return redirect('feedback')
+        except Exception as e:
+            error_msg = 'Server updating'
+            messages.error(request, error_msg)
+            return redirect('feedback')
     return render(request, 'feedback.html')
 
 def events(request):
