facc8a423505e099d49c631bf559382f46b3ce68
Rajeev <rajeevlakshmanan003@gmail.com>
2026-01-31 22:15:22 +0530

    Alpha test completed & ready for production.

--- Nexvent/urls.py
+++ Nexvent/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('core-admin/', admin.site.urls),
     path('', include('themes.urls')),
     path('events/', include('events.urls')),
     path('account_manager/', include('account_manager.urls')),
